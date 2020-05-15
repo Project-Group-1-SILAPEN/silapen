dfebddca2c23ba7762750892e03818334bb31344
ulfatunniswah11 <64625092+ulfatunniswah11@users.noreply.github.com>
2020-05-15 20:59:55 +0700

    Add files via upload
    revisi 1

--- tipe_lab.php
+++ tipe_lab.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class tipe_lab extends CI_Controller {
+class satuan extends CI_Controller {
 
 	public function __construct()
 	{
@@ -12,7 +12,7 @@ class tipe_lab extends CI_Controller {
 
 	public function index()
 	{
-		$this->fungsi->check_previleges('tipe_lab');
+		$this->fungsi->check_previleges('tipr_lab');
 		$data['tipe_lab'] = $this->m_tipe_lab->getData();
 		$this->load->view('master/tipe_lab/v_tipe_lab_list',$data);
     }
@@ -28,6 +28,35 @@ class tipe_lab extends CI_Controller {
 		}else{
 			$base_kom=$this->uri->segment(5);
 			$this->fungsi->run_js('load_silent("master/tipe_lab/show_editForm/'.$base_kom.'","#divsubcontent")');	
+		}
+	}
+
+	public function show_addForm()
+	{
+		$this->fungsi->check_previleges('tipe_lab');
+		$this->load->library('form_validation');
+		$config = array(
+				array(
+					'field'	=> 'tipe_lab',
+					'label' => 'tipe_lab',
+					'rules' => 'required'
+				)
+			);
+		$this->form_validation->set_rules($config);
+		$this->form_validation->set_error_delimiters('<span class="error-span">', '</span>');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['status']='';
+			$this->load->view('master/tipe_lab/v_tipe_lab_add',$data);
+		}
+		else
+		{
+			$datapost = get_post_data(array('kode','nama_tipe_lab','keterangan','id_lab'));
+			$this->m_tipe_lab->insertData($datapost);
+			$this->fungsi->run_js('load_silent("master/tipe_lab","#content")');
+			$this->fungsi->message_box("Data Master Tipe Lab sukses disimpan...","success");
+			$this->fungsi->catat($datapost,"Menambah Master tipe lab dengan data sbb:",true);
 		}
 	}
 }
