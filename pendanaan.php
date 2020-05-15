d1d1516a427c496f77f1222c7197c627e66f7763
Ryanis Naufalia <64945613+ryanfelia@users.noreply.github.com>
2020-05-15 20:37:01 +0700

    master pindah ke lab

--- pendanaan.php
+++ pendanaan.php
@@ -1,19 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class pendanaan extends CI_Controller {
+class Pendanaan extends CI_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
 		$this->fungsi->restrict();
-		$this->load->model('master/m_pendanaan');
+		$this->load->model('master/m_instansi');
 	}
 
 	public function index()
 	{
-		$this->fungsi->check_previleges('pendanaan');
-		$data['pendanaan'] = $this->m_pendanaan->getData();
-		$this->load->view('master/pendanaan/v_pendanaan_list',$data);
-    }
+		$this->fungsi->check_previleges('instansi');
+		$data['row'] = $this->m_instansi->getEdit('1')->row();
+		$this->load->view('master/instansi/v_instansi',$data);
+	}
 }
+/* End of file instansi.php */
+/* Location: ./application/controllers/master/instansi.php */
