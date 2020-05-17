274a2a2f2ed9749a73e075cfbad939766824a3f0
Ryanis Naufalia <64945613+ryanfelia@users.noreply.github.com>
2020-05-17 16:59:55 +0700

    revisi m_pendanaan

--- m_pendanaan.php
+++ m_pendanaan.php
@@ -1,62 +1,35 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-    <div class="row" id="form_pembelian">
-      <div class="col-lg-12">
-        <div class="box box-primary">
-          <div class="box-header with-border">
-            <h3 class="box-title">Master pendanaan</h3>
+class M_pendanaan extends CI_Model {
 
-            <div class="box-tools pull-right">
-            <?php
-              $sesi = from_session('level');
-              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
-                echo button('load_silent("master/pendanaan/form/base","#modal")','Add New pendanaan','btn btn-success');
-              } else {
-                # code...
-              }
-              ?>
-            </div>
-          </div>
-          <div class="box-body">
-            <table width="100%" id="tableku" class="table table-striped">
-              <thead>
-                <th>id</th>
-                <th>sumber_pendanaan</th>
-                <th>jumlah</th>
-                <th>keterangan</th>
-              </thead>
-              <tbody>
-          <?php 
-          $i = 1;
-          foreach($nama_alat->result() as $row): ?>
-          <tr>
-            <td align="center"><?=$i++?></td>
-            <td align="center"><?=$row->sumber_pendanaan?></td>
-            <td align="center"><?=$row->jumlah?></td>
-            <td align="center"><?=$row->keterangan?></td>
-            <td align="center">
-            <?php
-              $sesi = from_session('level');
-              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
-                echo button('load_silent("master/pendanaan/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
-              } else {
-                # code...
-              }
-              ?>
-            </td>
-          </tr>
+	public function getData($value='')
+	{
+		$this->db->from('master_pendanaan ma');
+		$this->db->order_by('ma.id', 'desc');
+		return $this->db->get();
+	}
 
-        <?php endforeach;?>
-        </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-<script type="text/javascript">
-  $(document).ready(function() {
-    var table = $('#tableku').DataTable( {
-      "ordering": false,
-    } );
-  });
-</script>
+	public function insertData($data='')
+	{
+		
+        $this->db->insert('master_pendanaan',$data);
+       
+	}
+
+	public function updateData($data='')
+	{
+		 $this->db->where('id',$data['id']);
+            $this->db->update('master_pendanaan',$data);
+	}
+
+	public function deleteData($id='')
+	{
+		$this->db->where('id', $id);
+        $this->db->delete('master_pendanaan');
+	}
+
+}
+
+/* End of file m_pendanaan.php */
+/* Location: ./application/models/master/m_pendanaan.php */
