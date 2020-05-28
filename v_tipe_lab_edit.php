8e70edbb4bf6eec0ae0704f6a63bc82bedc9e931
ulfatunniswah11 <64625092+ulfatunniswah11@users.noreply.github.com>
2020-05-28 15:56:59 +0700

    Add files via upload

--- v_tipe_lab_edit.php
+++ v_tipe_lab_edit.php
@@ -15,12 +15,11 @@
             </div>
         </div>
         <div class="form-group">
-            <label class="col-sm-4 control-label">nama_tipe_lab</label>
+            <label class="col-sm-4 control-label">Nama Tipe Lab</label>
             <div class="col-sm-8">
-            <?php echo form_input(array('name'=>'nama_tipe_lab','value'=>$row->nama_type_lab,'class'=>'form-control'));?>
+            <?php echo form_input(array('name'=>'nama_tipe_lab','value'=>$row->nama_tie_lab,'class'=>'form-control'));?>
             <?php echo form_error('nama_tipe_lab');?>
             </div>
-        </div>
         <div class="form-group">
             <label class="col-sm-4 control-label">Keterangan</label>
             <div class="col-sm-8">
@@ -28,9 +27,18 @@
             <?php echo form_error('keterangan');?>
             </div>
         </div>
-        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Save</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/tipe_lab/show_editForm/","#divsubcontent")','Save','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
     </form>
 </div>
+
+
 <script type="text/javascript">
 $(document).ready(function() {
     $(".select2").select2();
