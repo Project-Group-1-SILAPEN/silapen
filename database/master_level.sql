c4d7c2ccb19fb0e1e3d143da3c3e0f5dd3a69218
ulfatunniswah11 <ulfatunniswah@students.unnes.ac.id>
2020-06-25 22:24:05 +0700

    db

--- database/master_level.sql
+++ database/master_level.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2020 at 04:40 AM
+-- Generation Time: Jun 25, 2020 at 05:21 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sionlab`
+-- Database: `silapen`
 --
 
 -- --------------------------------------------------------
