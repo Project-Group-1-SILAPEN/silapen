2d6cb6e535475d47359d48d5bf17472cf4b4b944
ulfatunniswah11 <ulfatunniswah@students.unnes.ac.id>
2020-06-25 22:06:52 +0700

    bagus

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -20,16 +20,19 @@
                 ?>
                 <?php echo $avatar; ?>
                     <h5 class="description-header">SELAMAT DATANG DI <?php echo $this->config->item('project')?></h5>
-                    <span class="description-text"><?php echo button('load_silent("cms/user/show_editForm_user/'.from_session('id').'","#content")','Update Profil','btn bg-purple','data-toggle="tooltip" title="Update Profil"');?> 
+                    <span class="description-text"><?php echo button('load_silent("cms/user/show_editForm_user/'.from_session('id').'","#content")','Update Profil','btn bg-blue','data-toggle="tooltip" title="Update Profil"');?> 
 						</span>
                   </div>
+                 
                   <!-- /.description-block -->
                 </div>
                 
                 <!-- /.col -->
               </div>
+             
               <!-- /.row -->
             </div>
           </div>
           <!-- /.widget-user -->
+          <?php require ('application/views/kotak.php'); ?>
         </div>
